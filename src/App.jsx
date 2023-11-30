20495654c0a3d8d44a64a4db94ef611e75b0c4a9
farkhanmhd <farkhanmuhammadv3@gmail.com>
2023-11-30 15:26:27 +0700

    initital commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import Navigation from "./components/Navigation";
+
+const App = () => {
+  return (
+    <Fragment>
+      <Navigation />
+    </Fragment>
+  );
+};
+
+export default App;
