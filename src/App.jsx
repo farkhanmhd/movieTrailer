a98f3b048649c2b5ef41f7d304ae29833ea32f7e
farkhanmhd <farkhanmuhammadv3@gmail.com>
2023-12-01 16:07:30 +0700

    navigation finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
-import { Fragment } from "react";
-import Navigation from "./components/Navigation";
+import Navigation from "./components/Navigation/Navigation";
+import { useEffect, useState } from "react";
 
 const App = () => {
+  const [isDark, setIsDark] = useState(false);
+  useEffect(() => {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.documentElement.classList.add("dark");
+      setIsDark(true);
+    } else {
+      document.documentElement.classList.remove("dark");
+      setIsDark(false);
+    }
+  }, []);
+
+  const handleThemeToggle = () => {
+    if (isDark) {
+      document.documentElement.classList.remove("dark");
+      localStorage.theme = "light";
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.theme = "dark";
+    }
+    setIsDark(!isDark);
+    console.log(isDark);
+  };
+
   return (
-    <Fragment>
-      <Navigation />
-    </Fragment>
+    <div className="app bg-slate-300 dark:bg-zinc-900 min-w-full min-h-full h-screen">
+      <Navigation isDark={isDark} themeToggle={handleThemeToggle} />
+    </div>
   );
 };
 
